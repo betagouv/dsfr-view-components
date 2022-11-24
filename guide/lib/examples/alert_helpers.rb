8946bfa6230c678da8eb24e8da6e2862493881a8
Adrien Di Pasquale <adrien@dipasquale.fr>
2022-11-24 10:09:25 +0100

    review fixes

--- guide/lib/examples/alert_helpers.rb
+++ guide/lib/examples/alert_helpers.rb
@@ -2,21 +2,25 @@ module Examples
   module AlertHelpers
     def alert_success_with_content
       <<~ALERT
-        = dsfr_alert(type: :success, title: "Bravo bien joué") do |alert|
-          | Belle partie !
+        = dsfr_alert(type: :success, title: "Succès d'inscription") do |alert|
+          | Votre inscription a bien été enregistrée
       ALERT
     end
 
     def alert_error_sm_without_content
       <<~ALERT
-        = dsfr_alert(type: :error, size: :sm, title: "Erreur db 123")
+        = dsfr_alert( \
+          type: :error, \
+          size: :sm, \
+          title: "Une erreur est survenue pendant votre inscription, veuillez contacter votre administrateur." \
+        )
       ALERT
     end
 
     def alert_with_close_button
       <<~ALERT
         = dsfr_alert(type: :success, title: "Bienvenue", close_button: true) do
-          | Merci de vous être inscrit·e
+          | Merci pour votre inscription
       ALERT
     end
   end
