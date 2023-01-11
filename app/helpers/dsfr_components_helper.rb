78cb4a6e3e8b8655b34f7414d442ff3e98f6772c
Adrien Di Pasquale <adrien@dipasquale.fr>
2023-01-11 08:51:14 +0100

    component tile

--- app/helpers/dsfr_components_helper.rb
+++ app/helpers/dsfr_components_helper.rb
@@ -4,6 +4,7 @@ module DsfrComponentsHelper
     dsfr_alert: 'DsfrComponent::AlertComponent',
     dsfr_accordion: 'DsfrComponent::AccordionComponent',
     dsfr_accordion_section: 'DsfrComponent::AccordionComponent::SectionComponent',
+    dsfr_tile: 'DsfrComponent::TileComponent',
     # DO NOT REMOVE: new component mapping here
   }.each do |name, klass|
     define_method(name) do |*args, **kwargs, &block|
---
+++ guide/lib/examples/tile_helpers.rb
@@ -0,0 +1,53 @@
+module Examples
+  module TileHelpers
+    def tile_default
+      <<~RAW
+        = dsfr_tile(title: "Orgue de Fontainebleau",
+                    url: "#",
+                    image_src: "/assets/images/orgue.jpg",
+                    description: "Magistral instrument de musique",
+                    html_attributes: {style: "max-width: 20rem;"})
+      RAW
+    end
+
+    def tile_without_description
+      <<~RAW
+        = dsfr_tile(title: "Orgue de Fontainebleau",
+                    url: "#",
+                    image_src: "/assets/images/orgue.jpg",
+                    html_attributes: {style: "max-width: 20rem;"})
+      RAW
+    end
+
+    def tile_without_image
+      <<~RAW
+        = dsfr_tile(title: "Orgue de Fontainebleau",
+                    url: "#",
+                    description: "Magistral instrument de musique",
+                    html_attributes: {style: "max-width: 20rem;"})
+      RAW
+    end
+
+    def tile_with_image_alt
+      <<~RAW
+        = dsfr_tile(title: "Orgue de Fontainebleau",
+                    url: "#",
+                    image_src: "/assets/images/orgue.jpg",
+                    image_alt: "Orgue orange et bleu",
+                    description: "Magistral instrument de musique",
+                    html_attributes: {style: "max-width: 20rem;"})
+      RAW
+    end
+
+    def tile_horizontal
+      <<~RAW
+        = dsfr_tile(title: "Orgue de Fontainebleau",
+                    url: "#",
+                    image_src: "/assets/images/orgue.jpg",
+                    description: "Magistral instrument de musique",
+                    orientation: :horizontal,
+                    html_attributes: {style: "max-width: 30rem;"})
+      RAW
+    end
+  end
+end
