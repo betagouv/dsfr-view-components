e8b8bce1ee688a077550035d0f61263e8866ec67
Antoine Girard <dev@antoinegirard.eu>
2025-02-03 14:28:46 +0100

    Add more links, fix ul class and fix dsfr view component supported versions

--- guide/lib/examples/skiplink_helpers.rb
+++ guide/lib/examples/skiplink_helpers.rb
@@ -2,7 +2,7 @@ module Examples
   module SkiplinkHelpers
     def skiplink_default
       <<~RAW
-        - links = [dsfr_link_to("Aller au contenu", "#main-content")]
+        - links = [dsfr_link_to("En-tête", "#header"), dsfr_link_to("Contenu", "#main-content"), dsfr_link_to("Pied de page", "#footer")]
         = dsfr_skiplink(label: "Accès rapide", links: links)
       RAW
     end
