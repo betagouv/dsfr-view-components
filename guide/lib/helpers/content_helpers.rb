efbaf6447d975c0b51d6a43bc29a0376652726f3
Antoine Girard <dev@antoinegirard.eu>
2025-02-25 11:15:08 +0100

    Review

--- guide/lib/helpers/content_helpers.rb
+++ guide/lib/helpers/content_helpers.rb
@@ -11,8 +11,8 @@ module Helpers
 
     def dsfr_component_doc_links(doc_id = nil, storybook_id = nil)
       {
-        "Voir dans la documentation officielle" => "#{DSFR_COMPONENT_DOC_HREF}/#{doc_id}/",
-        "Voir sur le Storybook officiel" => "#{DSFR_COMPONENT_STORYBOOK_HREF}/?path=/docs/#{storybook_id}--docs"
+        "Voir #{@item[:title]} dans la documentation officielle" => "#{DSFR_COMPONENT_DOC_HREF}/#{doc_id}/",
+        "Voir #{@item[:title]} sur le Storybook officiel" => "#{DSFR_COMPONENT_STORYBOOK_HREF}/?path=/docs/#{storybook_id}--docs"
       }
     end
   end
