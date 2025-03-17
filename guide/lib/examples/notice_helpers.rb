59431f5345381953d53900fdd2adbdc24af7579e
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-03-17 18:48:11 +0100

    Add custom icon, custom dismiss label and add specs

--- guide/lib/examples/notice_helpers.rb
+++ guide/lib/examples/notice_helpers.rb
@@ -6,9 +6,15 @@ module Examples
       RAW
     end
 
+    def notice_alert
+      <<~RAW
+        = dsfr_notice(title: "Alerte", description: "Ceci est une alerte importante", type: "alert")
+      RAW
+    end
+
     def notice_dismissible
       <<~RAW
-        = dsfr_notice(title: "Information", description: "Ceci est une info importante que vous pouvez masquer", dismissible: true)
+        = dsfr_notice(title: "Information", description: "Ceci est une info importante que vous pouvez masquer", dismissible: true, dismiss_label: "Masquer")
       RAW
     end
 
@@ -21,6 +27,18 @@ module Examples
     def notice_with_internal_link
       <<~RAW
         = dsfr_notice(title: "Information", description: "Ceci est une info importante avec un super lien", link_label: "En savoir plus", link_href: "#", link_blank: false)
+      RAW
+    end
+
+    def notice_without_icon
+      <<~RAW
+        = dsfr_notice(title: "Information", description: "Ceci est une info importante", icon: false)
+      RAW
+    end
+
+    def notice_with_icon_name
+      <<~RAW
+        = dsfr_notice(title: "Information", description: "Ceci est une info importante", icon_name: "alarm-warning-fill")
       RAW
     end
   end
