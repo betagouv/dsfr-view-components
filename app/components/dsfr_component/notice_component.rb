aa7c9a7e1d4d02a6a7a42f1b91b49a8d3405aeea
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-03-17 18:48:11 +0100

    Add custom icon, custom dismiss label and add specs

--- app/components/dsfr_component/notice_component.rb
+++ app/components/dsfr_component/notice_component.rb
@@ -1,34 +1,47 @@
 module DsfrComponent
   class NoticeComponent < DsfrComponent::Base
-    TYPES = %i[info waring alert weather-orange weather-purple kidnapping cyberattack vigipirate witness].freeze
+    GENERIC_TYPES = %w[info waring alert].freeze
+    WEATHER_TYPES = %w[weather-orange weather-red weather-purple].freeze
+    ALERT_TYPES = %w[kidnapping cyberattack attack witness].freeze
+    TYPES = GENERIC_TYPES + WEATHER_TYPES + ALERT_TYPES
+
+    WEATHER_ICONS = %w[windy-fill thunderstorms-fill heavy-showers-fill flood-fill temp-cold-fill sun-fill avalanches-fill snowy-fill].freeze
+
     MARKUPS = %i[p h1 h2 h3 h4 h5 h6].freeze
 
     # @param title [String] Titre du bandeau
     # @param description [String] Description du bandeau pour apporter du contexte (optionnel)
-    # @param type [Symbol] Type de bandeau (info, waring, alert, weather-orange, weather-purple, kidnapping, cyberattack, vigipirate, witness), par défaut :info
+    # @param type [String] Type de bandeau (info, waring, alert, weather-orange, weather-red, weather-purple, kidnapping, cyberattack, attack, witness), par défaut "info"
     # @param markup [Symbol] Balise HTML à utiliser pour la description (p, h1, h2, h3, h4, h5, h6)
-    # @param icon [Boolean] Afficher ou non une icône
+    # @param icon [Boolean] Afficher ou non une icône, uniquement pour les bandeaux génériques, par défaut true
     # @param icon_name [String] Nom de l'icône à afficher
     # @param notice [Boolean] Ajout l’attribut role="notice" (si insertion du bandeau à la volée), par défaut false
     # @param dismissible [Boolean] Ajouter un bouton de fermeture, par défaut false
+    # @param dismiss_label [String] Libellé du bouton de fermeture (optionnel, par défaut « Masquer le message »)
     # @param link_label [String] Libellé du lien (optionnel)
     # @param link_href [String] URL du lien (optionnel)
     # @param link_title [String] Titre du lien (optionnel)
     # @param link_blank [Boolean] Ouvrir le lien dans un nouvel onglet, par défaut true
-    def initialize(title:, description:, type: :info, markup: :p, icon: true, icon_name: nil, notice: false, dismissible: false, link_label: nil, link_href: nil, link_title: nil, link_blank: true,
+    def initialize(title:, description:, type: "info", markup: :p, icon: true, icon_name: nil, notice: false, dismissible: false, dismiss_label: "Masquer le message", link_label: nil, link_href: nil, link_title: nil, link_blank: true,
                    classes: [], html_attributes: {})
       @title = title
       @description = description
       @type = type
       @markup = markup
-      @icon = icon
       @icon_name = icon_name
       @notice = notice
       @dismissible = dismissible
+      @dismiss_label = dismiss_label
       @link_label = link_label
       @link_href = link_href
       @link_title = link_title
       @link_blank = link_blank
+
+      @icon = if GENERIC_TYPES.include?(type)
+                icon
+              else
+                true
+              end
 
       super(classes: classes, html_attributes: html_attributes)
     end
@@ -38,7 +51,7 @@ module DsfrComponent
     attr_reader :type, :title, :description, :markup, :icon, :icon_name, :notice, :dismissible, :link_label, :link_href, :link_title, :link_blank
 
     def type_class
-      raise ArgumentError, "Invalid type: #{type}" unless type_valid?
+      raise ArgumentError, "Invalid type: #{type}. Valid types: #{TYPES.join(', ')}" unless type_valid?
 
       "fr-notice--#{type}"
     end
@@ -64,11 +77,28 @@ module DsfrComponent
     def link_tag
       return unless link_display?
 
-      link_to(link_label, link_href, title: link_title, target: link_blank ? "_blank" : "_self")
+      target = @link_blank ? "_blank" : "_self"
+
+      link_to(link_label, link_href, title: link_title, target: target)
+    end
+
+    def icon_class
+      return unless icon_name
+
+      raise ArgumentError, "L’icône n’est pas personnalisable sur les bandeaux d’alertes" if ALERT_TYPES.include?(type)
+      raise ArgumentError, "L’icône d’un bandeau de type météo doit être une icône météo (#{WEATHER_ICONS.join(', ')})" if WEATHER_TYPES.include?(type) && !WEATHER_ICONS.include?(icon_name)
+
+      "fr-icon-#{icon_name}"
     end
 
     def default_attributes
-      { class: "fr-notice #{type_class}" }
+      class_attr = "fr-notice #{type_class}"
+      class_attr += " fr-notice--no-icon" unless icon
+
+      attr = { class: class_attr }
+      attr[:role] = "notice" if notice
+
+      attr
     end
   end
 end
--- spec/components/dsfr_component/notice_component_spec.rb
+++ spec/components/dsfr_component/notice_component_spec.rb
@@ -1,5 +1,190 @@
 require 'spec_helper'
 
 RSpec.describe(DsfrComponent::NoticeComponent, type: :component) do
-  pending "Add some examples to cover NoticeComponent"
+  subject { render_inline(described_class.new(**args)) }
+
+  let(:args) { { title: "Information", description: "Ceci est un message d'information" } }
+
+  it "renders correctly" do
+    subject
+
+    expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--info" }) do
+      with_tag(:div, with: { class: "fr-container" }) do
+        with_tag(:div, with: { class: "fr-notice__body" }) do
+          with_tag(:p) do
+            with_tag(:span, with: { class: "fr-notice__title" }, text: "Information")
+            with_tag(:span, with: { class: "fr-notice__desc" }, text: "Ceci est un message d'information")
+          end
+          without_tag(:button, with: { class: "fr-btn--close fr-btn", type: "button", title: "Fermer" })
+        end
+      end
+    end
+  end
+
+  context "when type is alert" do
+    let(:args) { { title: "Alerte", description: "Ceci est un message d'alerte", type: "alert" } }
+
+    it "renders correctly" do
+      subject
+
+      expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--alert" }) do
+        with_tag(:div, with: { class: "fr-container" }) do
+          with_tag(:div, with: { class: "fr-notice__body" }) do
+            with_tag(:p) do
+              with_tag(:span, with: { class: "fr-notice__title" }, text: "Alerte")
+              with_tag(:span, with: { class: "fr-notice__desc" }, text: "Ceci est un message d'alerte")
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context "when dismissible" do
+    let(:args) { { title: "Information", description: "Ceci est un message d'information", dismissible: true, dismiss_label: "Fermer" } }
+
+    it "renders correctly" do
+      subject
+
+      expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--info" }) do
+        with_tag(:div, with: { class: "fr-container" }) do
+          with_tag(:div, with: { class: "fr-notice__body" }) do
+            with_tag(:button, with: { class: "fr-btn--close fr-btn", type: "button", title: "Fermer" }, text: "Fermer")
+          end
+        end
+      end
+    end
+  end
+
+  context "when link is present" do
+    let(:args) { { title: "Information", description: "Ceci est un message d'information", link_label: "En savoir plus", link_href: "https://example.com", link_title: "En savoir plus - Nouvel onglet" } }
+
+    it "renders correctly" do
+      subject
+
+      expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--info" }) do
+        with_tag(:div, with: { class: "fr-container" }) do
+          with_tag(:div, with: { class: "fr-notice__body" }) do
+            with_tag(:p) do
+              with_tag(:a, with: { href: "https://example.com", title: "En savoir plus - Nouvel onglet", target: "_blank" }, text: "En savoir plus")
+            end
+          end
+        end
+      end
+    end
+
+    context "when link_blank is false" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", link_label: "En savoir plus", link_href: "#", link_title: "En savoir plus", link_blank: false } }
+
+      it "renders correctly" do
+        subject
+
+        expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--info" }) do
+          with_tag(:div, with: { class: "fr-container" }) do
+            with_tag(:div, with: { class: "fr-notice__body" }) do
+              with_tag(:p) do
+                with_tag(:a, with: { href: "#", title: "En savoir plus", target: "_self" }, text: "En savoir plus")
+              end
+            end
+          end
+        end
+      end
+    end
+
+    context "when icon is false" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", icon: false } }
+
+      it "renders correctly" do
+        subject
+
+        expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--info fr-notice--no-icon" })
+      end
+    end
+
+    context "when icon is false and notice has weather type" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", type: "weather-red", icon: false } }
+
+      it "renders correctly" do
+        subject
+
+        expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--weather-red" })
+      end
+    end
+
+    context "when icon is false and notice has alert type" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", type: "attack", icon: false } }
+
+      it "renders correctly" do
+        subject
+
+        expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--attack" })
+      end
+    end
+
+    context "when icon_name is present" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", icon_name: "alarm-warning-fill" } }
+
+      it "renders correctly" do
+        subject
+
+        expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--info" }) do
+          with_tag(:div, with: { class: "fr-container" }) do
+            with_tag(:div, with: { class: "fr-notice__body" }) do
+              with_tag(:p) do
+                with_tag(:span, with: { class: "fr-notice__title fr-icon-alarm-warning-fill" }, text: "Information")
+              end
+            end
+          end
+        end
+      end
+    end
+
+    context "when notice has weather type and icon_name is invalid" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", type: "weather-red", icon_name: "alarm-warning-fill" } }
+
+      it "raise" do
+        expect do
+          subject
+        end.to raise_error(ArgumentError, "L’icône d’un bandeau de type météo doit être une icône météo (windy-fill, thunderstorms-fill, heavy-showers-fill, flood-fill, temp-cold-fill, sun-fill, avalanches-fill, snowy-fill)")
+      end
+    end
+
+    context "when notice has weather type and icon_name is valid" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", type: "weather-red", icon_name: "windy-fill" } }
+
+      it "renders correctly" do
+        subject
+
+        expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--weather-red" }) do
+          with_tag(:div, with: { class: "fr-container" }) do
+            with_tag(:div, with: { class: "fr-notice__body" }) do
+              with_tag(:p) do
+                with_tag(:span, with: { class: "fr-notice__title fr-icon-windy-fill" }, text: "Information")
+              end
+            end
+          end
+        end
+      end
+    end
+
+    context "when notice has alert type and icon_name is present" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", type: "attack", icon_name: "alarm-warning-fill" } }
+
+      it "raise" do
+        expect do
+          subject
+        end.to raise_error(ArgumentError, "L’icône n’est pas personnalisable sur les bandeaux d’alertes")
+      end
+    end
+
+    context "when notice has notice role" do
+      let(:args) { { title: "Information", description: "Ceci est un message d'information", notice: true } }
+
+      it "renders correctly" do
+        subject
+
+        expect(rendered_content).to have_tag(:div, with: { class: "fr-notice fr-notice--info", role: "notice" })
+      end
+    end
+  end
 end
