14b5f474df621d3d990a967823c4577c1c028d2b
Goulven Champenois <goulven.champenois@userland.fr>
2025-04-15 17:40:53 +0200

    Prevent a warning from crashing docs generation

--- app/components/dsfr_component/search_bar_component.rb
+++ app/components/dsfr_component/search_bar_component.rb
@@ -22,7 +22,7 @@ module DsfrComponent
 
       validate_size!
 
-      super(classes: nil, html_attributes: html_attributes)
+      super(classes: [], html_attributes: html_attributes)
     end
 
   private
