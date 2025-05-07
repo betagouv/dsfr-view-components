f72bcea2cac933e4a875143052eee3b18ef0da71
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2025-05-07 17:44:22 +0200

    highlight: allow passing a block for content

--- app/components/dsfr_component/highlight_component.rb
+++ app/components/dsfr_component/highlight_component.rb
@@ -2,19 +2,20 @@ module DsfrComponent
   class HighlightComponent < DsfrComponent::Base
     # @param text [String] Le contenu textuel du composant
     # @param size [Symbol] La taille de la mise en exergue
-    def initialize(text:, size: :md, html_attributes: {})
+    def initialize(text: nil, size: :md, html_attributes: {})
       @text = text
       @size = size
-
-      raise ArgumentError if not SIZES.include?(size)
 
       super(html_attributes: html_attributes)
     end
 
     def call
+      raise(ArgumentError, "You must provide either a text param or some block content") if no_content?
+      raise(ArgumentError, "Size '#{size}' is not allowed; possible values are #{SIZES}") if SIZES.exclude?(size)
+
       tag.div(**html_attributes) do
         tag.p(class: "fr-text--#{@size}") do
-          @text
+          @text || content
         end
       end
     end
@@ -25,6 +26,10 @@ module DsfrComponent
 
     def default_attributes
       { class: 'fr-highlight' }
+    end
+
+    def no_content?
+      text.blank? && !content?
     end
   end
 end
--- spec/components/dsfr_component/highlight_component_spec.rb
+++ spec/components/dsfr_component/highlight_component_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe(DsfrComponent::HighlightComponent, type: :component) do
-  context "when an unknown size is passed" do
-    it "raises an error" do
-      expect { render_inline(described_class.new(text: "foo", size: :xz)) }.to raise_error ArgumentError
+  subject(:rendered_content) do
+    if content.present?
+      render_inline(component.with_content(content)).to_s
+    else
+      render_inline(component).to_s
     end
   end
 
-  it "generates the right markup" do
-    render_inline(described_class.new(text: "foo"))
+  let(:text) { "foo" }
+  let(:size) { :md }
+  let(:content) { nil }
+  let(:kwargs) { { text: text, size: size } }
+  let(:component_css_class) { "fr-highlight" }
+  let(:component) { described_class.new(**kwargs) }
 
+  it "generates the right markup" do
     expect(rendered_content).to have_tag(:div, with: { class: "fr-highlight" }) do
       with_tag(:p, with: { class: "fr-text--md" }) do
         "foo"
@@ -17,11 +24,35 @@ RSpec.describe(DsfrComponent::HighlightComponent, type: :component) do
     end
   end
 
-  context "when a different size is specified" do
-    it "produces the corresponding markup" do
-      render_inline(described_class.new(text: "foo", size: :lg))
+  context "when an unknown size is passed" do
+    let(:size) { :xz }
 
+    it_behaves_like 'a component that fails to render', /Size 'xz' is not allowed/
+  end
+
+  context "when a different size is specified" do
+    let(:size) { :lg }
+
+    it "produces the corresponding markup" do
       expect(rendered_content).to have_tag(:p, with: { class: "fr-text--lg" })
     end
   end
+
+  context "when a block is provided" do
+    let(:content) { "hello" }
+
+    it "renders that content" do
+      expect(rendered_content).to have_tag(:p, with: { class: "fr-text--md" }) do
+        "hello"
+      end
+    end
+  end
+
+  context "without a block or a text" do
+    let(:text) { nil }
+
+    it_behaves_like 'a component that fails to render', /either a text param or some block/
+  end
+
+  it_behaves_like 'a component that accepts custom HTML attributes'
 end
