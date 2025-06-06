4e83ea5d4b021547c7a437bf45e98cc1581cf2af
Goulven Champenois <goulven.champenois@userland.fr>
2025-06-06 15:23:51 +0200

    feat: Allow linking to components in beta

--- guide/lib/helpers/content_helpers.rb
+++ guide/lib/helpers/content_helpers.rb
@@ -1,6 +1,7 @@
 module Helpers
   module ContentHelpers
     DSFR_COMPONENT_DOC_HREF = "https://www.systeme-de-design.gouv.fr/composants-et-modeles/composants".freeze
+    DSFR_COMPONENT_BETA_DOC_HREF = "https://www.systeme-de-design.gouv.fr/composants-et-modeles/composants-beta".freeze
     DSFR_COMPONENT_STORYBOOK_HREF = "https://storybook.systeme-de-design.gouv.fr".freeze
 
     def site_title
@@ -9,9 +10,9 @@ module Helpers
 
   private
 
-    def dsfr_component_doc_links(doc_id = nil, storybook_id = nil)
+    def dsfr_component_doc_links(doc_id = nil, storybook_id = nil, beta: false)
       {
-        "Voir #{@item[:title]} dans la documentation officielle" => "#{DSFR_COMPONENT_DOC_HREF}/#{doc_id}/",
+        "Voir #{@item[:title]} dans la documentation officielle" => "#{beta ? DSFR_COMPONENT_BETA_DOC_HREF : DSFR_COMPONENT_DOC_HREF}/#{doc_id}/",
         "Voir #{@item[:title]} sur le Storybook officiel" => "#{DSFR_COMPONENT_STORYBOOK_HREF}/?path=/docs/#{storybook_id}--docs"
       }
     end
