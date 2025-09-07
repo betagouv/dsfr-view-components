378bdf4af47be240703b98bcb8888d74e6b89e0c
Goulven Champenois <goulven.champenois@userland.fr>
2025-09-07 16:40:29 +0200

    fix: Remove storybook links since it has been merged into documentation

--- guide/lib/helpers/content_helpers.rb
+++ guide/lib/helpers/content_helpers.rb
@@ -1,7 +1,6 @@
 module Helpers
   module ContentHelpers
     DSFR_COMPONENT_DOC_HREF = "https://www.systeme-de-design.gouv.fr/version-courante/fr/composants/".freeze
-    DSFR_COMPONENT_STORYBOOK_HREF = "https://storybook.systeme-de-design.gouv.fr".freeze
 
     def site_title
       "Composants Rails ViewComponent pour le Système de Design de lʼÉtat"
@@ -9,10 +8,9 @@ module Helpers
 
   private
 
-    def dsfr_component_doc_links(doc_id = nil, storybook_id = nil)
+    def dsfr_component_doc_links(doc_id = nil)
       {
-        "Voir #{@item[:title]} dans la documentation officielle" => "#{DSFR_COMPONENT_DOC_HREF}#{doc_id}",
-        "Voir #{@item[:title]} sur le Storybook officiel" => "#{DSFR_COMPONENT_STORYBOOK_HREF}/?path=/docs/#{storybook_id}--docs"
+        "Voir #{@item[:title]} dans la documentation officielle" => "#{DSFR_COMPONENT_DOC_HREF}#{doc_id}"
       }
     end
   end
