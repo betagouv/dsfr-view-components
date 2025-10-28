ef85461265eebc5fa45804702713b87d4d9f2986
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-10-28 19:16:08 +0100

    Stepper : ajout de la possibilité de changer le niveau de titre

--- app/components/dsfr_component/stepper_component.rb
+++ app/components/dsfr_component/stepper_component.rb
@@ -4,23 +4,30 @@ module DsfrComponent
     # @param value [Integer] Numéro de l’étape en cours (commence à 1)
     # @param max [Integer] Nombre d’étapes total
     # @param next_title [String] Titre de l’étape suivante (sauf pour la dernière étape)
-    def initialize(title:, value:, max:, next_title: nil, html_attributes: {})
+    # @param heading_level [Integer] Niveau du titre
+    def initialize(title:, value:, max:, next_title: nil, heading_level: 2, html_attributes: {})
       @title = title
       @value = value
       @max = max
       @next_title = next_title
+      @heading_level = heading_level
 
       raise ArgumentError, "Les étapes doivent aller de 1 jusqu´à 8 au maximum" if @value < 1 || @value > @max || @max > 8
+      raise ArgumentError, "Le niveau du titre doit être compris entre 1 et 6" if HEADING_LEVELS.exclude?(heading_level)
 
       super(html_attributes: html_attributes)
     end
 
   private
 
-    attr_reader :title, :value, :max, :next_title
+    attr_reader :title, :value, :max, :next_title, :heading_level
 
     def default_attributes
       { class: 'fr-stepper' }
+    end
+
+    def title_tag(*args, **kwargs, &block)
+      content_tag("h#{heading_level}", *args, **kwargs, &block)
     end
   end
 end
